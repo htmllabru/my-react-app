88f6a86940c617e7a3d331fb78ca40990ac63de3
Aleksei Tarasov <htmllabru@gmail.com>
2025-03-30 22:54:07 +0300

    Add emoji

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          счётчик {count}
+          счётчик 👉 {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
